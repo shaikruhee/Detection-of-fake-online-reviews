2a664a0fa8935d0db5ce126dd8b3d381df798db2
shaik Ruhee <ruheeshaik177@gmail.com>
2025-03-26 21:47:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 # Install system dependencies
 apt-get update && apt-get install -y python3-pip python3-venv build-essential python3-dev
 
+# Create and activate virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
 # Install Python packages
 pip install -r requirements.txt
+pip install joblib
 
 # Run the Streamlit app
 streamlit run app.py
